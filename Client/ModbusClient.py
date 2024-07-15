21b7fb05c5319a5b30f6ec2c3c58b85195720f16
aditd <aditd1993@gmail.com>
2024-07-16 01:27:12 +0300

    add redis connection and post to client reports index

--- Client/ModbusClient.py
+++ Client/ModbusClient.py
@@ -1,10 +1,11 @@
-import os
-import pyModbusTCP
 from pyModbusTCP.client import ModbusClient
 import time
-import random
 import threading
+import json
+from datetime import datetime
 import logging
+import redis
+from elasticsearch import Elasticsearch, helpers
 
 # init clients from outside list and remote registers define
 file_path='ModbusServers.txt'
@@ -15,6 +16,17 @@ WATER_PUMP_ADDR = 0         # Coils address for water tank's pump status
 
 # define log file
 logging.basicConfig(level=logging.INFO, filename="/var/log/OT/ModbusClient.log", filemode="w", format='%(asctime)s - %(levelname)s - %(message)s')
+
+# define remote redis cluster / container
+redis_host = 'eesgi10.ee.bgu.ac.il'
+redis_port=6379
+redis_index='ModbusClientReports'
+try:
+    r = redis.Redis(host=redis_host,port=redis_port,decode_responses=True)
+    logging.info(f"Info: Connection to redis container {redis_host}:{redis_port} has established")
+except redis.ConnectionError as e:
+    logging.error(f"Error: Redis connection has failed: {e}")
+    exit(1)
 
 class Session(threading.Thread):
     def __init__(self,server_ip,server_port,unit_id):
@@ -34,6 +46,27 @@ class Session(threading.Thread):
             else:
                 print(f"    Connected to server: {self.server_ip}, ID: {self.unit_id}")
                 logging.info(f"Info: connected to server: {self.server_ip}, ID: {self.unit_id}")
+
+    def post_to_redis(self, water_level, high_mark_state, low_mark_state,pump_state):
+        # post to redis whenever a water pump status changes
+        data = {
+            'timestamp': datetime.now().isoformat(),
+            'server_data': {
+                'Server_IP': self.server_ip,
+                'Server_port': self.server_port,
+                'Unit_id': self.unit_id,
+                'Water_level': water_level,
+                'High_threshold_sensor': high_mark_state,
+                'Low_threshold_sensor': low_mark_state,
+                'Water_pump_status': pump_state
+            }
+        }
+        try:
+            r.lpush(redis_index,json.dumps(data))
+            logging.info(f"Info: Post to redis complete")
+        except redis.PubSubError as e:
+            logging.error(f"Error: {e}")
+
 
 
     def run(self):
@@ -56,12 +89,14 @@ class Session(threading.Thread):
                                     self.client.write_single_coil(WATER_PUMP_ADDR,True)
                                     print(f"Turn IP: {self.server_ip}, ID: {self.unit_id} water pump ON")
                                     logging.info(f"Info: Turn IP: {self.server_ip}, ID: {self.unit_id} water pump ON")
+                                    self.post_to_redis(water_level,high_mark_state,low_mark_state,pump_state)
                             # turn off pump
                             case (high_mark_state, low_mark_state, pump_state) if high_mark_state and low_mark_state:
                                 if pump_state == True:
                                     self.client.write_single_coil(WATER_PUMP_ADDR,False)
                                     print(f"Turn IP: {self.server_ip}, ID: {self.unit_id} water pump OFF")
                                     logging.info(f"Info: Turn IP: {self.server_ip}, ID: {self.unit_id} water pump OFF")
+                                    self.post_to_redis(water_level,high_mark_state,low_mark_state,pump_state)
 
                     else:
                         print("Can't read water level input register from server")
